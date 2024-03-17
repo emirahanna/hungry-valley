895055f24df460a394403c524e9fa995f61c45cf
Emira Y <emirahanna@gmail.com>
2024-03-17 07:04:56 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,12 @@ import "./styles.css";
 import MapsBasic from "./components/basicmap";
 
 import "leaflet/dist/leaflet.css";
-import {MapContainer, TileLayer} from 'react-leaflet';
-import { useState } from 'react'
 
 export default function App() {
-  return (
-    <div className="App">
-      <Header />
-    <MapContainer center={[40.798214, -77.859909]} zoom={13}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright"
-        OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      </MapContainer>
-  
-  </div>
-  );
-
+	return (
+		<div className="App">
+			<Header />
+			<MapsBasic />
+		</div>
+	);
 }
