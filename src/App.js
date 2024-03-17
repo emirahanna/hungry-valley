535d0c3931eb9c65de4ab527756e29fab7778e12
Emira Y <emirahanna@gmail.com>
2024-03-17 03:15:04 -0400

    Update Header.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,6 @@ export default function App() {
   return (
     <div className="App">
       <Header />
-      <MapContainer center={[40.798214, -77.859909]} zoom={15}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright"
-          OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
     <MapContainer center={[40.798214, -77.859909]} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright"
