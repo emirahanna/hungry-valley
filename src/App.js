80d49d6e490003921de73e9134eb339f63e8973a
Emira Y <emirahanna@gmail.com>
2024-03-17 08:42:13 -0400

    We have Auth0 now

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import "./App.css";
 import Header from "./components/Header";
 import "./styles.css";
 import MapsBasic from "./components/basicmap";
-
 import "leaflet/dist/leaflet.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 
 export default function App() {
 	return (
-		<div className="App">
+		<main className="App">
+			<h1> Auth0 Login</h1>
+			<LoginButton />
+			<LogoutButton />
+      <Profile />
 			<Header />
 			<MapsBasic />
-		</div>
+		</main>
 	);
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,10 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Profile = () => {
+    const { user, isAuthenticated} = useAuth0();
+    
+return (
+    isAuthenticated && (
+       <article className="column"> {JSON.stringify(user)}</article>)
+    )}
+export default Profile;
