ebd1f49fcb002efc0ac97dd291b7d194c23ea9f7
Huzaifah Fakhrul Anuar <hxf5155@psu.edu>
2024-03-17 04:04:34 -0400

    added functional node.js and mongodb implementation, working well with react

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import Header from './components/Header';
 import "./styles.css";
 import "leaflet/dist/leaflet.css";
 import {MapContainer, TileLayer} from 'react-leaflet';
-
+import { useState } from 'react'
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const handleOnSubmit = async (e) => {
+      e.preventDefault();
+      let result = await fetch(
+      'http://localhost:5000/register', {
+          method: "post",
+          body: JSON.stringify({ name, email }),
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      })
+      result = await result.json();
+      console.warn(result);
+      if (result) {
+          alert("Data saved succesfully");
+          setEmail("");
+          setName("");
+      }}
   return (
     <div className="App">
       <Header />
@@ -23,7 +42,14 @@ export default function App() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       </MapContainer>
-  
+      <form action="">
+                <input type="text" placeholder="name"
+                value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="email" placeholder="email"
+                value={email} onChange={(e) => setEmail(e.target.value)} />
+                <button type="submit"
+                onClick={handleOnSubmit}>submit</button>
+            </form>
   </div>
   );
 
