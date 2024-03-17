441a1edddc9f5597c835171876d99fbee476598c
Emira Y <emirahanna@gmail.com>
2024-03-16 23:02:29 -0400

    Update Header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button>Sign in here pls</button>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Header(){
-    return (
-        <header>
-            <h1>Hungry Valley</h1>
-        </header>
-    );
+class Header extends Component {
+	render() {
+
+        const headerStyle ={  
+            backgroundColor: "#282c34",
+            minHeight: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center"};
+		return html`
+			<div style = {headerStyle}>
+				<h1>Hungry Valley</h1>
+                <button>Sign in here pls</button>
+			</div>
+		`;
+	}
 }
-
 export default Header;
