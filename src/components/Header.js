398412584d3a045dc776a34a1212c0c40bfdd1ce
Emira Y <emirahanna@gmail.com>
2024-03-17 11:23:48 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,22 @@ import "./Header.css";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 import Profile from "./Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Header(){
+    const { isLoading, error } = useAuth0();
     return (
         <header className="header">
             <h1 className="title">HungryValley</h1>
-			<LoginButton />
-			<LogoutButton />
-            <Profile />
+            {error && <p>Authentication Error</p>}
+            {!error && isLoading && <p>Loading...</p>}
+			{!error && !isLoading && (
+                <>
+                <LoginButton />
+                <LogoutButton />
+                <Profile />
+                </>
+            )}
 
         </header>
     );
