44ce36fd2c27a1265dbe04e729a6c422ac4fbc84
fatinaqida <spm6834@psu.edu>
2024-03-17 02:30:06 -0400

    Auth0 intergration

--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+import LoginButton from "../components/login";
+import LogoutButton from "../components/logout";
+import Profile from "../components/profile";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const IndexPage = () => (
+  <Auth0Provider
+      domain="dev-ujbug0lrq88yile8.us.auth0.com"
+      clientId="7rChKF3lHO64GABJwP2MTJYuEGk1CIuU"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App>
+        <div className="App">
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+        <button>Sign in here pls</button>
+        </div>
+      </App>
+    </Auth0Provider>
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
